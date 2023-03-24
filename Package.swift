e864e5a6812c696245b7539536eff151123d901a
Andrew Hershberger <andrew.hershberger@superhuman.com>
2023-03-23 23:38:16 -0400

    initial commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ActionsErrorDemoDependency",
+    platforms: [.iOS(.v9), .macOS("10.9"), .tvOS(.v9), .watchOS(.v2)],
+    products: [
+        .library(
+            name: "ActionsErrorDemoDependency",
+            targets: ["ActionsErrorDemoDependency"]),
+    ],
+    targets: [
+        .target(
+            name: "ActionsErrorDemoDependency",
+            publicHeadersPath: "public")
+    ],
+    cxxLanguageStandard: .cxx11
+)
---
+++ Sources/ActionsErrorDemoDependency/src/a.cpp
@@ -0,0 +1,8 @@
+#include "a.h"
+
+
+void foo()
+{
+    return;
+}
+
